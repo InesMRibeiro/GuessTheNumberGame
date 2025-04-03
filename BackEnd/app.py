c86e8b687569503eacc4398c5de69e12c534403e
InesMRibeiro <inesmribeiro4102@gmail.com>
2025-04-04 00:21:15 +0100

    Alterações de IPS e no VPC

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import random
 
 app = Flask(__name__)
-CORS(app, resources={"/*": {"origins": "http://44.211.240.240:5000"}})  # Allowing only your frontend
+CORS(app, resources={"/*": {"origins": "http://54.175.90.182"}})  # Allowing only your frontend
 
 # Game logic
 class Game:
